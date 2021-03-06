94c0c0577884f0f6b887ffd0d712b7bb38e14add
Yuval Marmer <yuvalmarmer97@gmail.com>
2021-03-06 13:33:37 +0200

    init

---
+++ Snowman/snowman.cpp
@@ -0,0 +1,11 @@
+#include "snowman.hpp"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+namespace ariel{
+        string snowman(int number){
+            return "Hello";
+        }
+};
