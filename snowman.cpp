ba4e012f6734fa7dad791d5f18b32bf8cd1237ba
Yuval Marmer <yuvalmarmer97@gmail.com>
2021-03-10 11:49:25 +0200

    Adding functonality

--- snowman.cpp
+++ snowman.cpp
@@ -20,6 +20,9 @@ X(TTT)Y
 
 namespace ariel{
 
+    bool leftArmFlag = false;
+    bool rightArmFlag = false;
+
     //Hat
     void Hat(char (*skelton)[WIDTH], int num){
         switch (num)
@@ -44,7 +47,7 @@ namespace ariel{
             break;
         
         default:
-            break;
+            throw out_of_range("The code " + to_string(num) + " is out of range");
         }
     }
 
@@ -52,28 +55,159 @@ namespace ariel{
     //Left arm
     void LeftArm(char (*skelton)[WIDTH], int num){
 
-        for(int i=0;i<HEIGHT;i++){
-            for(int j=0;j<WIDTH;j++){
-                cout << skelton[i][j];
-            }
-            cout << endl;
+        switch (num)
+        {
+            case 1:
+                skelton[2][0] = ' ';
+                skelton[3][0] = '<';
+                break;
+            case 2:
+                skelton[2][0] = '\\';
+                skelton[3][0] = ' ';
+                break;
+
+            case 3:
+                skelton[2][0] = ' ';
+                skelton[3][0] = '/';
+                break;
+
+            case 4:
+                skelton[2][0] = ' ';
+                skelton[3][0] = ' ';
+                leftArmFlag=true;
+                break;
+            
+            default:
+                throw out_of_range("The code " + to_string(num) + " is out of range");
+        }
+    }
+    
+    //Right arm
+    void RightArm(char (*skelton)[WIDTH], int num){
+        switch (num)
+        {
+            case 1:
+                skelton[2][6] = ' ';
+                skelton[3][6] = '>';
+                break;
+            case 2:
+                skelton[2][6] = '/';
+                skelton[3][6] = ' ';
+                break;
+
+            case 3:
+                skelton[2][6] = ' ';
+                skelton[3][6] = '\\';
+                break;
+
+            case 4:
+                skelton[2][6] = ' ';
+                skelton[3][6] = ' ';
+                rightArmFlag=true;
+                break;
+            
+            default:
+                throw out_of_range("The code " + to_string(num) + " is out of range");
         }
     }
 
-    //Right arm
-    void RightArm(char (*skelton)[WIDTH], int num){}
-
     //Left eye
-    void LeftEye(char (*skelton)[WIDTH], int num){}
+    void LeftEye(char (*skelton)[WIDTH], int num){
+        switch (num)
+        {
+            case 1:
+                skelton[2][2] = '.';
+                break;
+            case 2:
+                skelton[2][2] = 'o';
+                break;
+
+            case 3:
+                skelton[2][2] = 'O';
+                break;
+
+            case 4:
+                skelton[2][2] = '-';
+                break;
+            
+            default:
+                throw out_of_range("The code " + to_string(num) + " is out of range");
+        }
+    }
 
     //Right eye
-    void RightEye(char (*skelton)[WIDTH], int num){}
+    void RightEye(char (*skelton)[WIDTH], int num){
+        switch (num)
+        {
+            case 1:
+                skelton[2][4] = '.';
+                break;
+            case 2:
+                skelton[2][4] = 'o';
+                break;
+
+            case 3:
+                skelton[2][4] = 'O';
+                break;
+
+            case 4:
+                skelton[2][4] = '-';
+                break;
+            
+            default:
+                throw out_of_range("The code " + to_string(num) + " is out of range");
+        }
+    }
 
     //Nose
-    void Nose(char (*skelton)[WIDTH], int num){}
+    void Nose(char (*skelton)[WIDTH], int num){
+        switch (num)
+        {
+            case 1:
+                skelton[2][3] = ',';
+                break;
+            case 2:
+                skelton[2][3] = '.';
+                break;
+
+            case 3:
+                skelton[2][3] = '_';
+                break;
+
+            case 4:
+                skelton[2][3] = ' ';
+                break;
+            
+            default:
+                throw out_of_range("The code " + to_string(num) + " is out of range");
+        }
+    }
 
     //Torso
-    void Torso(char (*skelton)[WIDTH], int num){}
+    void Torso(char (*skelton)[WIDTH], int num){
+        switch (num)
+        {
+            case 1:
+                strcpy(skelton[3]+2 , " : ");
+                //skelton[3][3] = ',';
+                break;
+            case 2:
+                strcpy(skelton[3]+2 , "] [");
+                break;
+
+            case 3:
+                strcpy(skelton[3]+2 , "> <");
+                break;
+
+            case 4:
+                strcpy(skelton[3]+2 , "   ");
+                break;
+            
+            default:
+                throw out_of_range("The code " + to_string(num) + " is out of range");
+        }
+    }
+    
 
     //Base
     void Base(char (*skelton)[WIDTH], int num){}
@@ -100,6 +234,13 @@ namespace ariel{
 
 
         } while (temp);
+
+        for (int i=0;i<HEIGHT;i++){
+            for(int j=0;j<WIDTH;j++){
+                cout << skelton[i][j];
+            }
+            cout << endl;
+        }
 
 
         if(number_of_digits>8)
